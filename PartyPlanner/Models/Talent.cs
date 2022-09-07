a5d0feed1253f388a367a5f308e83a296c906f28
Edgar Luque <git@edgarluque.com>
2022-09-07 20:38:23 +0200

    update to 1.3.2

---
+++ PartyPlanner/Models/Talent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyPlanner.Models
+{
+    public class Talent
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string LongSecription { get; set; }
+
+        // todo: do this
+    }
+}
